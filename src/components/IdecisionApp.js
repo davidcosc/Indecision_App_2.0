af529cf4d23e5ca93f0cf89ccf77d4987a0e0ef6
david <d.cornel@tu-braunschweig.de>
2018-04-29 12:51:56 +0200

    Added pickOption. Changed: IndecisionApp.js, Action.js

--- src/components/IdecisionApp.js
+++ src/components/IdecisionApp.js
@@ -3,11 +3,19 @@ import Header from './Header';
 import Action from './Action';
 
 export default class IndecisionApp extends React.Component {
+    state = {
+        options: ['1', '2'],
+    };
+    pickOption = () => {
+        const optionIndex = Math.floor(Math.random() * this.state.options.length);
+        const option = this.state.options[optionIndex];
+        alert(option);
+    };
     render() {
         return (
             <div>
                 <Header title="IndecisionApp_2.0" subtitle="Put your life in the hands of a computer"/>
-                <Action hasOption={false}/>
+                <Action hasOption={this.state.options.length > 0} pickOption={this.pickOption}/>
             </div>
         );
     }
