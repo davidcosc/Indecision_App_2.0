a00c550803a9e6bcf41af7413c4b57d93b18e81e
david <d.cornel@tu-braunschweig.de>
2018-04-29 13:58:12 +0200

    Added Option. Changed: Option.js, Options.js

---
+++ src/components/Option.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Option = (props) => (
+    <div>
+        <p>{props.option}</p>
+        <button>Remove</button>
+    </div>
+);
+export default Option;
--- src/components/Options.js
+++ src/components/Options.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import Option from './Option';
 
 const Options = (props) => (
     <div>
         <button disabled={!props.hasOption} onClick={props.clearOptions}>Clear options</button>
         {props.options.length === 0 && <p>Enter some options to get started</p>}
-        {props.options.map((option) => <p key={option}>{option}</p>)}
+        {props.options.map((option) => <Option key={option} option={option}/>)}
     </div>
 );
 export default Options;
