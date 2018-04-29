ce6f662259b7e34c9223b3e986f742d697f71a74
david <d.cornel@tu-braunschweig.de>
2018-04-29 12:42:46 +0200

    Added Header. Changed: IndecisionApp.js, Header.js

--- src/components/IdecisionApp.js
+++ src/components/IdecisionApp.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import Header from './Header';
 
 export default class IndecisionApp extends React.Component {
     render() {
         return (
             <div>
-                test
+                <Header title="IndecisionApp_2.0" subtitle="Put your life in the hands of a computer"/>
             </div>
         );
     }
