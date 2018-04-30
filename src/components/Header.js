31a3f7966e145b7ba41bdae11477c4208bd660cc
david <d.cornel@tu-braunschweig.de>
2018-04-30 19:04:17 +0200

    Added header styles. Changed: _header.scss, header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,11 @@
 import React from 'react';
 
 const Header = (props) => (
-    <div>
-        <h1>{props.title}</h1>
-        {!!props.subtitle && <h2>{props.subtitle}</h2>}
+    <div className="header">
+        <div className="header__content">
+            <h1 className="header__content__title">{props.title}</h1>
+            {!!props.subtitle && <h2 className="header__content__subtitle">{props.subtitle}</h2>}
+        </div>
     </div>
 );
 export default Header;
